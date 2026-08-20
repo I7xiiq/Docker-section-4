5603668ee093aa03dcd805166969a9856df4e701
I7xiiq <am9248968@gmail.com>
2026-08-20 13:01:39 +0300

    add mongo connection

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 //init app
 const app = express();
 const PORT = 4000 // add port 
+
+
+//connect to db
+const DB_USER = 'root';
+const DB_PASSWORD = 'example';
+const DB_PORT = 27017;
+const DB_HOST = 'mongo';
+
+const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`
+mongoose.connect(URI)
+.then(() => console.log('connect to db..'))
+.catch((err) => console.log('faild connect to db' , err));
 
 app.get('/', (req, res) => res.send('<h1> Hello Docker!</h1>'));
 
